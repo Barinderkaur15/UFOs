a5f71adfdd6b997a1b301b0612a96b167fb17bc0
Barinderkaur15 <55926650+Barinderkaur15@users.noreply.github.com>
2019-12-21 15:09:24 -0800

    Adding Challenge JS file

--- js/app.js
+++ js/app.js
@@ -2,36 +2,61 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+//Function for creating the table 
 function buildTable(data) {
-    // First, clear out any existing data
     tbody.html("");
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-      );
-    });
-  }
-function handleClick() {
-let date = d3.select("#datetime").property("value");
-let filteredData = tableData;l
-// Check to see if a date was entered and filter the
-  // data using that date.
-  if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    filteredData = filteredData.filter(row => row.datetime === date);
-  };
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
 
-  buildTable(filteredData);
-};
-d3.select("#filter-btn").on("click", handleClick);
+        });
+
+    });
+
+}
+//Function to handle multiple filters
+function updateFilters() {
+    let date = d3.select("#datetime").property("value");
+    let city = d3.select("#cityname").property("value");
+    let state = d3.select("#statename").property("value");
+    let country = d3.select("#countryname").property("value");
+    let shape = d3.select("#shape").property("value"); 
+
+    let filteredData = tableData;
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (city) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `city` value matches the filter value
+        filteredData = filteredData.filter(row => row.city === city);
+    }
+    if (state) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `state` value matches the filter value
+        filteredData = filteredData.filter(row => row.state === state);
+    }
+    if (country) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `country` value matches the filter value
+        filteredData = filteredData.filter(row => row.country === country);
+    }
+    if (shape) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `shape` value matches the filter value
+        filteredData = filteredData.filter(row => row.shape === shape);
+    }
+
+    buildTable(filteredData);
+
+}
+
+
+d3.selectAll("#filter-btn").on("click", updateFilters);
+
 // Build the table when the page loads
 buildTable(tableData);
